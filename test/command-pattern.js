e14254afd8550b3083449de5953bd4350c0933f6
Yernende <yernende@gmail.com>
2015-05-04 18:41:24 +0600

    Implement CommandPattern#test

---
+++ test/command-pattern.js
@@ -0,0 +1,12 @@
+import {assert} from "chai";
+import CommandPattern from "../lib/command-pattern";
+
+describe("CommandPattern", () => {
+    describe("#test", () => {
+        it("should return whether a passed string matches a pattern or not", () => {
+            let lookPattern = new CommandPattern("look");
+            assert.ok(lookPattern.test("look"));
+            assert.notOk(lookPattern.test("smile"));
+        });
+    });
+});
