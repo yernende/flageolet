75e25e3283e469cd44bbd44679408faa2222ecae
Yernende <yernende@gmail.com>
2015-05-12 20:39:09 +0600

    Implement Command#test

--- src/command.js
+++ src/command.js
@@ -1,3 +1,11 @@
+import CommandPattern from "./command-pattern";
+
 export default class Command {
-	
+	constructor(pattern) {
+		this.pattern = new CommandPattern(pattern);
+	}
+
+	test(string) {
+		return this.pattern.test(string);
+	}
 }
--- test/command.js
+++ test/command.js
@@ -1,5 +1,11 @@
+import {assert} from "chai";
 import Command from "../lib/command";
 
 describe("Command", () => {
-
+	describe("#test", () => {
+		it("should return whether a passed string matches a command's pattern or not", () => {
+			let lookCommand = new Command("look");
+			assert.ok(lookCommand.test("look"));
+		});
+	});
 });
