c4ba119e3b1ccc96edaf18995270055c4c58723f
Yernende <yernende@gmail.com>
2015-06-08 12:32:31 +0600

    Implement User#acceptQuery

--- src/user.js
+++ src/user.js
@@ -1,3 +1,13 @@
-export default class User {
+import stream from "stream";
 
+export default class User {
+	constructor() {
+		this.queries = new stream.PassThrough();
+	}
+
+	acceptQuery() {
+		return new Promise((resolve) => {
+			this.queries.once("data", (data) => resolve(data));
+		});
+	}
 }
--- test/user.js
+++ test/user.js
@@ -1,6 +1,23 @@
 import {assert} from "chai";
 import User from "../lib/user";
+import stream from "stream";
 
 describe("User", () => {
+	describe("#acceptQuery", () => {
+		describe("result", () => {
+			it("should be a next user#queries query pushed after the call", () => {
+				return new Promise((resolve, reject) => {
+					let user = new User();
+					let queryToSend = "smile";
 
+					user.acceptQuery().then((query) => {
+						assert.equal(query, queryToSend);
+						resolve();
+					}).catch(reject);
+
+					user.queries.write(queryToSend);
+				});
+			});
+		});
+	});
 });
