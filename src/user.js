8b4868ba7157c290b0ecdb6a63bf40e05e504025
Yernende <yernende@gmail.com>
2015-06-16 16:36:05 +0600

    Refactor User#request

--- src/user.js
+++ src/user.js
@@ -1,36 +1,17 @@
 import stream from "stream";
+import {LineStream} from "byline";
 
 export default class User {
 	constructor() {
 		this.queries = new stream.PassThrough();
-		this.request = new Request(this);
+		this.request = new stream.PassThrough();
+
+		this.request.pipe(new LineStream()).pipe(this.queries);
 	}
 
 	acceptQuery() {
 		return new Promise((resolve) => {
 			this.queries.once("data", (data) => resolve(data.toString()));
 		});
-	}
-}
-
-class Request extends stream.Writable {
-	constructor(user) {
-		super();
-		this._user = user;
-		this._buffer = "";
-	}
-
-	_write(chunk, encoding, done) {
-		let data = chunk.toString();
-
-		for (let symbol of data) {
-			if (symbol == "\n") {
-				this._user.queries.write(this._buffer);
-			} else {
-				this._buffer += symbol;
-			}
-		}
-
-		done();
 	}
 }
