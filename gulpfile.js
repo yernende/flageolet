dfa2869e60fe7d8f81bcd23ad8269ee0660c2631
Yernende <yernende@gmail.com>
2015-07-08 19:19:48 +0600

    gulp transpile: reimplement the watch mode
    Now `gulp transpile` with the `--watch` (`-w`) option should be called
    to watch sources and incrementally retranspile on changes.

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,29 @@ import gulp from "gulp";
 import babel from "gulp-babel";
 import plumber from "gulp-plumber";
 import notifier from "node-notifier";
+import minimist from "minimist";
+import watch from "gulp-watch";
 
 const PATHS = {
-	DESTINATION: "./lib",
-	SOURCES: "./src/**/*"
+	DESTINATION: "lib",
+	SOURCES: "src/**/*"
 }
 
+const argv = minimist(process.argv, {
+	boolean: ["watch"],
+	alias: {
+		w: "watch"
+	}
+});
+
 gulp.task("transpile", () => {
-	gulp.src(PATHS.SOURCES)
+	let src = gulp.src(PATHS.SOURCES);
+
+	if (argv.watch) {
+		src = src.pipe(watch(PATHS.SOURCES))
+	}
+
+	src
 		.pipe(plumber({
 			errorHandler: (error) => {
 				console.log(`${error.name}: ${error.message}`);
@@ -24,6 +39,8 @@ gulp.task("transpile", () => {
 		.pipe(babel())
 		.pipe(plumber.stop())
 		.pipe(gulp.dest(PATHS.DESTINATION));
+
+	return src;
 });
 
 gulp.task("default", ["transpile"]);
