e3d2814f333cb7303be737312c09ba4751419f87
Yernende <yernende@gmail.com>
2015-07-18 17:01:52 +0600

    Transmit `Room#events` input objects to the room's members

--- src/room.js
+++ src/room.js
@@ -1,5 +1,16 @@
+import stream from "stream";
+
 export default class Room {
 	constructor() {
 		this.members = new Set();
+
+		this.events = new stream.Writable({
+			objectMode: true,
+			write: (data, encoding, done) => {
+				for (let member of this.members) {
+					member.events.write(data);
+				}
+			}
+		});
 	}
 }
--- test/room.js
+++ test/room.js
@@ -1,6 +1,28 @@
 import {assert} from "chai";
 import Room from "../lib/room";
 
-describe("Room", () => {
+import Character from "../lib/character";
 
+describe("Room", () => {
+	describe("#events", () => {
+		it("should transmit input objects to its members", () => {
+			return new Promise((resolve, reject) => {
+				let room = new Room();
+				let character = new Character();
+				let eventToSend = {
+					name: "notification",
+					data: "hello"
+				};
+
+				character.move(room);
+
+				character.events.on("data", (event) => {
+					assert.equal(event, eventToSend);
+					resolve();
+				});
+
+				room.events.write(eventToSend);
+			});
+		});
+	});
 });
