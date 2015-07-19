a968848f776823ef6446ba460cd3424fd2ef2272
Yernende <yernende@gmail.com>
2015-07-19 09:05:55 +0600

    Implement `Item#move` similar to `Character#move`

--- src/item.js
+++ src/item.js
@@ -1,3 +1,10 @@
 export default class Item {
+	move(destination) {
+		if (this.location) {
+			this.location.members.delete(this);
+		}
 
+		this.location = destination;
+		destination.members.add(this);
+	}
 }
