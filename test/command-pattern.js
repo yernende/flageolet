812c6461f731f6abf3f54a1363d3d9f7c7c28608
Yernende <yernende@gmail.com>
2015-09-01 15:58:22 +0600

    Refactor CommandPattern tests

--- test/command-pattern.js
+++ test/command-pattern.js
@@ -18,16 +18,16 @@ describe("CommandPattern", () => {
 			assert.notOk(commandPatterns.quit.test("quit the world"));
 		});
 
-		describe("a word in angle brackets", () => {
-			it("should match a single arbitrary word", () => {
+		describe("when the pattern includes a word in angle brackets", () => {
+			it("should treat the word as matching a single arbitrary word", () => {
 				assert.ok(commandPatterns.get.test("get sword"));
 			});
 
-			it("should match several arbitrary single-quoted words", () => {
+			it("should treat the word as matching several arbitrary single-quoted words", () => {
 				assert.ok(commandPatterns.get.test("get 'rusty sword'"));
 			});
 
-			it("should match several arbitrary double-quoted words", () => {
+			it("should treat the word as matching several arbitrary double-quoted words", () => {
 				assert.ok(commandPatterns.get.test("get \"rusty sword\""));
 			});
 		});
@@ -47,16 +47,27 @@ describe("CommandPattern", () => {
 		});
 
 		describe("if a string passed that matches the pattern", () => {
-			describe("result", () => {
-				it("should be an object", () => {
-					assert.isObject(commandPatterns.get.exec("get sword"));
-				});
+			it("should return an object", () => {
+				assert.isObject(commandPatterns.look.exec("look"));
+			});
+		});
 
-				it("should store parameter values by parameter names as keys", () => {
-					let parameters = commandPatterns.get.exec("get 'rusty sword'");
+		describe("when the pattern includes a word in angle brackets", () => {
+			it("should treat the word as matching a single arbitrary word", () => {
+				assert.ok(commandPatterns.get.test("get sword"));
+			});
 
-					assert.equal(parameters.item, "rusty sword");
-				});
+			it("should treat the word as matching several arbitrary single-quoted words", () => {
+				assert.ok(commandPatterns.get.test("get 'rusty sword'"));
+			});
+
+			it("should treat the word as matching several arbitrary double-quoted words", () => {
+				assert.ok(commandPatterns.get.test("get \"rusty sword\""));
+			});
+
+			it("should include the word's matched value to the method's result", () => {
+				let parameters = commandPatterns.get.exec("get 'rusty sword'");
+				assert.equal(parameters.item, "rusty sword");
 			});
 		});
 	});
