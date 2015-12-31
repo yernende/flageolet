f50198aed7d1aa53a6781d829f71a961eb3b1573
Yernende <yernende@gmail.com>
2015-12-31 21:48:20 +0600

    Remove `Room#events`

--- src/room.js
+++ src/room.js
@@ -5,14 +5,5 @@ import Destination from "./destination";
 export default class Room extends Destination {
 	constructor() {
 		super();
-
-		this.events = new stream.Writable({
-			objectMode: true,
-			write: (data, encoding, done) => {
-				for (let member of this.members) {
-					member.events.write(data);
-				}
-			}
-		});
 	}
 }
--- test/room.js
+++ test/room.js
@@ -6,27 +6,5 @@ import Character from "../lib/character";
 import describeAModel from "./shared/describe-a-model";
 
 describe("Room", () => {
-	describe("#events", () => {
-		it("should transmit input objects to its members", () => {
-			return new Promise((resolve, reject) => {
-				let room = new Room();
-				let character = new Character();
-				let eventToSend = {
-					name: "notification",
-					data: "hello"
-				};
-
-				character.move(room);
-
-				character.events.on("data", (event) => {
-					assert.equal(event, eventToSend);
-					resolve();
-				});
-
-				room.events.write(eventToSend);
-			});
-		});
-	});
-
 	describeAModel(Room);
 });
