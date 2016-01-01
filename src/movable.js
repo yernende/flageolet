119b69c437cbc83717569591a65a752de1062ee8
Yernende <yernende@gmail.com>
2016-01-01 16:34:23 +0600

    Subscribe to the destination on `Movable#move`

--- src/movable.js
+++ src/movable.js
@@ -4,9 +4,11 @@ export default class Movable extends Model {
 	move(destination) {
 		if (this.location) {
 			this.location.members.delete(this);
+			this.location.subscribers.delete(this);
 		}
 
 		this.location = destination;
 		destination.members.add(this);
+		destination.subscribers.add(this);
 	}
 }
--- test/shared/describe-a-movable.js
+++ test/shared/describe-a-movable.js
@@ -33,6 +33,14 @@ export default function describeAsMovable(entityName, getEntityInstance) {
 				entity.move(destination);
 				assert.ok(destination.members.has(entity));
 			});
+
+			it(`should add the ${entityName} to the ${destinationType.name}'s subscribers`, () => {
+			    let entity = getEntityInstance();
+			    let destination = destinationType.getInstance();
+
+			    entity.move(destination);
+			    assert.ok(destination.subscribers.has(entity));
+			});
 		}
 
 		it(`should delete the ${entityName} from its previous location members`, () => {
@@ -44,6 +52,17 @@ export default function describeAsMovable(entityName, getEntityInstance) {
 			entity.move(destination);
 
 			assert.notOk(location.members.has(entity));
+		});
+
+		it(`should delete the ${entityName} from its previous location subscribers`, () => {
+			let entity = getEntityInstance();
+			let location = new Room();
+			let destination = new Room();
+
+			entity.move(location);
+			entity.move(destination);
+
+			assert.notOk(location.subscribers.has(entity));
 		});
 	});
 }
