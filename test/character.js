125a594546380f36baec88051cdc5421c38ed86e
Yernende <yernende@gmail.com>
2016-01-01 16:23:01 +0600

    Cover `Character#inventory` with the tests of models

--- test/character.js
+++ test/character.js
@@ -8,6 +8,10 @@ import describeAMovable from "./shared/describe-a-movable";
 import describeAModel from "./shared/describe-a-model";
 
 describe("Character", () => {
+	describe("#inventory", () => {
+		describeAModel("inventory", () => new Character().inventory);
+	});
+
 	describeAMovable("character", () => new Character());
 	describeAModel("character", () => new Character());
 });
