483ea62d93a30c04c59b1a46070a47efc2b21e79
Yernende <yernende@gmail.com>
2016-09-24 15:25:51 +0600

    Make basic query handling

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
 const net = require("net");
+const User = require("./lib/user");
+
+const users = [];
 
 const server = net.createServer((connection) => {
-  connection.pipe(connection);
+  let user = new User(connection);
+
+  connection.on("data", (data) => {
+    user.input.push(data.toString());
+  });
+
+  connection.on("close", () => {
+    users.splice(users.indexOf(user), 1);
+  })
+
+  users.push(user);
 }).listen(4000);
+
+setInterval(function () {
+  for (let user of users) {
+    user.handleInput();
+    user.handleOutput();
+  }
+}, 125);
