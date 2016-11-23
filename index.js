ad2bc290ba1819b08473bfd504327649cbc34113
Yernende <yernende@gmail.com>
2016-11-23 16:14:29 +0600

    Make basic command parsing

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 const net = require("net");
 const fs = require("fs");
+const game = require("./lib/game");
 const User = require("./lib/user");
 const Command = require("./lib/command");
 
-const users = [];
-const commands = [];
-
 for (let file of fs.readdirSync("./commands")) {
-  commands.push(...require(`./commands/${file}`).map((command) => new Command(command)));
+  game.commands.push(...require(`./commands/${file}`).map((command) => new Command(command)));
 }
 
 const server = net.createServer((connection) => {
@@ -18,14 +16,14 @@ const server = net.createServer((connection) => {
   });
 
   connection.on("close", () => {
-    users.splice(users.indexOf(user), 1);
+    game.users.splice(game.users.indexOf(user), 1);
   })
 
-  users.push(user);
+  game.users.push(user);
 }).listen(4000);
 
 setInterval(function () {
-  for (let user of users) {
+  for (let user of game.users) {
     user.handleInput();
     user.handleOutput();
   }
