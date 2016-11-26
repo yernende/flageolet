c9a0d70f39cce2601318971fdafde2442650ce2b
Yernende <yernende@gmail.com>
2016-11-27 00:42:13 +0600

    Implement basic world and movement

--- commands/index.js
+++ commands/index.js
@@ -3,4 +3,39 @@ module.exports = [{
   action(string) {
     console.log(string);
   }
+}, {
+  pattern: "north",
+  action() {
+    this.character.move("north");
+  }
+}, {
+  pattern: "east",
+  action() {
+    this.character.move("east");
+  }
+}, {
+  pattern: "south",
+  action() {
+    this.character.move("south");
+  }
+}, {
+  pattern: "west",
+  action() {
+    this.character.move("west");
+  }
+}, {
+  pattern: "up",
+  action() {
+    this.character.move("up");
+  }
+}, {
+  pattern: "down",
+  action() {
+    this.character.move("down");
+  }
+}, {
+  pattern: "look",
+  action() {
+    this.output.push(this.character.location.name + "\n");
+  }
 }];
